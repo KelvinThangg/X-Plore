d21eef01a3b198c85543b2db477448bf464843bf
tunanhdapoet <167734054+tunanhdapoet@users.noreply.github.com>
2024-04-23 13:31:07 +0700

    fix

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace X_Plore
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            Application.EnableVisualStyles(); 
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormDangNhap());
         }
